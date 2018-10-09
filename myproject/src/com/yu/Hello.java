8d52dda5db45c390a67e87f660b0d07cf02d2fc2
user <user@es302n17>
2018-10-09 09:29:25 +0800

    123

---
+++ myproject/src/com/yu/Hello.java
@@ -0,0 +1,15 @@
+package com.yu;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		double height = Math.sin(3.14);
+		System.out.println(height);
+		double root = Math.sqrt(16.0);
+		System.out.printf("%.2f\n",root);
+		double radians = Math.sin(Math.toRadians(180.0));
+		System.out.println(radians);
+	}
+
+}
