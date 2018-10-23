ff1c956c0827eade44acbd69deaed420c50ac99f
user <user@es302n17>
2018-10-23 11:13:38 +0800

    修改RangeTester

--- myproject/src/com/yu/RangeTester.java
+++ myproject/src/com/yu/RangeTester.java
@@ -7,16 +7,10 @@ public class RangeTester {
 	public static void main(String[] args) {
 		Scanner in=new Scanner(System.in);
 		System.out.print("輸入一整數:");
-		boolean b;
 		int num=in.nextInt();
-		if(num<=5&&num>=-3){
-			b=true;
-		}
-		else{
-			b=false;
-		}
+		boolean b=num<=5&&num>=-3;
 		System.out.println(b);
-		
+
 	}
 
 }
--- myproject/src/com/yu/SignUp.java
+++ myproject/src/com/yu/SignUp.java
@@ -8,7 +8,7 @@ public class SignUp {
 		System.out.println("是否成年(y/N)");
 		Scanner in=new Scanner(System.in);
 		String line=in.nextLine();
-		boolean adutl=line.equals("y")||line.equals("Y");
+		boolean adutl=line.equalsIgnoreCase("y");
 		if(adutl){
 			System.out.print("輸入年齡:");
 			int age=in.nextInt();
@@ -19,7 +19,7 @@ public class SignUp {
 			System.out.println("您的資料如下:\n年齡:"+age+"\n姓名:"+name+"\n暱稱:"+namE);
 		}
 		else{
-			System.out.println("無法使用");
+			System.out.println("無法註冊");
 		}
 
 	}
